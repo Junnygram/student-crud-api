2bb23e358fcbfd8c4510346a491034b054a970af
Olaleye <90139191+Junnygram@users.noreply.github.com>
2024-07-08 09:59:41 +0100

    Update server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(studentRoutes);
 
-app.listen(port, () => {
+app.listen(port,'0.0.0.0' () => {
   console.log(`Server is running on port ${port}`);
 });
